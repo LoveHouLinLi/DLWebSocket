999f6ab5ff6e2945b8bd8facac954ee4e530623e
JamesKobe <jameskobe@JamesKobedeMacBook-Pro.local>
2015-06-30 19:52:51 +0800

    Initial Commit

---
+++ DLWebSocket/DLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLAppDelegate.h
+//  DLWebSocket
+//
+//  Created by JamesKobe on 15/6/30.
+//  Copyright (c) 2015年 杨德龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DLWebSocket/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLWebSocket
+//
+//  Created by JamesKobe on 15/6/30.
+//  Copyright (c) 2015年 杨德龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLAppDelegate class]));
+    }
+}
